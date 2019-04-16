325400569f5b7e65147dec65ee7050c07822df6e
Barkhadle <37410181+Barkhadle@users.noreply.github.com>
2019-04-16 12:08:33 +0300

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,19 @@
+public class Addition{
+	
+	public static void main(String[] arg){
+		int num1;
+		int num2;
+		num1=20;
+		num2=30;
+		System.out.print(num1+num2);
+		
+		System.out.print("\n.......................");
+		
+		String n1;
+		String n2;
+		n1="20";
+		n2="30";
+		System.out.print("\n"+n1+n2);
+	}
+
+}
